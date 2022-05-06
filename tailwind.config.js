6765a39b4ce42ee5952c4bc79600652131cd5a8c
broklyn777 <50321354+broklyn777@users.noreply.github.com>
2022-05-06 12:21:02 +0200

    add scrolling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
     extend: {},
   },
   plugins: [
-    require('tailwindcss-textshadow')
+    require('tailwindcss-textshadow'),
+    require('tailwind-scrollbar-hide'),
+    require('tailwind-scrollbar'),
+
+
+
   ],
 }
